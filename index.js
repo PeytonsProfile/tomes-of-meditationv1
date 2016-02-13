b16e208f3d4d31633390b788507c76533f5cb03f
Peyton Caseria <PeytonCaseria@gmail.com>
2016-02-12 17:09:21 -0700

    accordion, modal, navbar, finished. finishing PDFs, toc and gcse bar

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+(function() {
+    var cx = '011386288940278185258:eem9a9dhjki';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
